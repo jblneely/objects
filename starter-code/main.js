73445844e976cd211dae31f04acb5778f1797109
jblneely <bowman.neely@gmail.com>
2017-04-05 21:51:25 -0700

    Wednesday Homework

--- starter-code/main.js
+++ starter-code/main.js
@@ -4,6 +4,69 @@
 	Each of these animal objects should have a name, a noise that they make,
 	and a makeNoise property that prints out their noise.
 */
+var dog = {
+    name: "labrador",
+    kingdom: "animalia",
+    phylum: "chordata",
+    class: "mammalia",
+    order: "carnivora",
+    family: "canidae",
+    genus: "canis",
+    species: "lupus",
+    hunger: 1
+    noise: "bark",
+    makeNoise: function() {
+        return this.noise
+    }
+}
+
+var cat = {
+    name: "bengal",
+    kingdom: "animalia",
+    phylum: "chordata",
+    class: "mammalia",
+    order: "carnivora",
+    family: "felidae",
+    genus: "panthera",
+    species: "tigris",
+    hunger: 2
+    noise: "thePurr",
+    makeNoise: function() {
+        return this.noise
+    }
+}
+
+var bird = {
+    name: "bluejay",
+    kingdom: "animalia",
+    phylum: "chordata",
+    class: "aves",
+    order: "passeriformes",
+    family: "corvidae",
+    genus: "cyanocitta",
+    species: "cristata",
+    hunger: 3
+    noise: "theCall",
+    makeNoise: function() {
+        return this.noise
+    }
+}
+
+var mouse = {
+    name: "labMouse",
+    kingdom: "animalia",
+    phylum: "chordata",
+    class: "mammalia",
+    order: "rodentia",
+    family: "muridae",
+    genus: "mus",
+    species: "musculus",
+    hunger: 4
+    noise: "theSqueek",
+    makeNoise: function() {
+        return this.noise
+    }
+}
 
 /*
 	Part 2: Let's use an array to hold all of these animals.
@@ -13,7 +76,10 @@
 
 	Hint: Use the makeNoise() function for each animal in the array
 */
-
+var animals = [dog, cat, bird, mouse];
+for (i = 0; i <= animals.length; i++) {
+    console.log(animals[i].makeNoise(this))
+}
 /*
 	Part 3: Let's see how hungry these animals get by adding a "hunger" property to them.
 	Use a for-loop to iterate through the array of animals and add a "hunger" property with
@@ -24,81 +90,111 @@
 	myObject.newPropertyName = newValue; // or...
 	myObject['newPropertyName'] = newValue;
 */
+var animals = [dog, cat, bird, mouse];
+for (i = 0; i <= animals.length; i++) {
+    animals[i].hunger = 9
+        /*
+        	Part 4: These animals should be able to walk. Let's add a new "walk" property to them.
+        	For each animal, the "walk" property should be a function that prints out a string that
+        	says they are walking. It should also subtract 1 from their "hunger" property.
 
-/*
-	Part 4: These animals should be able to walk. Let's add a new "walk" property to them.
-	For each animal, the "walk" property should be a function that prints out a string that
-	says they are walking. It should also subtract 1 from their "hunger" property.
+        	For example, if I had a duck that looked like this:
+        	var duck = {
+        		name: 'duck',
+        		hunger: 8
+        	};
+        	duck.walk();
+        	// => "duck took a walk"
+        //
+        	console.log(duck.hunger);
+        	// => 7
 
-	For example, if I had a duck that looked like this:
-	var duck = {
-		name: 'duck',
-		hunger: 8
-	};
-	duck.walk();
-	// => "duck took a walk"
-//
-	console.log(duck.hunger);
-	// => 7
-
-	Hint: You will need the "this" keyword
-*/
-
-/*
-	Part 5: Now these animals can walk and talk by themselves, but they're quite lonely.
-	Let's add a "makeFriend" property to each animal. The "makeFriend" property will be a
-	method that takes another animal as an argument, and adds it to their "friend" property.
-
-	For example, if I had a duck and swan that looked like this:
-	var duck = {
-		name: 'duck'
-	};
-	var swan = {
-		name: 'swan',
-		noise: 'chirp quack'
-	};
-	duck.makeFriend(swan);
-
-	console.log(duck.friend);
-	// => { name: 'swan' }
-
-	duck.friend.makeNoise();
-	// => "chirp quack"
-*/
+        	Hint: You will need the "this" keyword
+        */
 
 
-/* BONUS
-This bonus will be a challenge! To complete this step, you'll need to do some Googling to look up things that have not yet been covered.
 
-Take a look at the index.html file. You'll notice the following code:
 
-	<img src="">
-	
-Here we have an image without a src attribute. 
+    for (i = 0; i < animals.length; i++) {
+        animals[i].walk = function() {
+            console.log(this.name + "took a walk!");
+            this.hunger--;
+        }
+    }
 
-Now take a peek inside the images folder to see what images are there. 
+    //this.hunger = (this.hunger - 1)
 
-Here are the image paths for reference:
 
-	- images/bird.jpg
-	- images/cat.jpg
-	- images/dog.jpg
-	- images/mouse.jpg
 
-Note that the name of each image is the animal name (bird, cat, dog, or mouse) plus an extension of .jpg.
+    // Part 5: Now these animals can walk and talk by themselves, but they're quite lonely.
+    // Let's add a "makeFriend" property to each animal. The "makeFriend" property will be a
+    // method that takes another animal as an argument, and adds it to their "friend" property.
 
-Now that you're familiar with the code and assets that are available to you, let's add an appear property to each animal. The appear property will be a method that updates the src attribute to the path of the image of that animal.
+    // For example, if I had a duck and swan that looked like this:
 
-Test things out by calling the appear method for one of your animals. When you load the page in your browser, the image of the animal should appear!
 
-Hint #1: Google the following:
-	- "JavaScript querySelector"
-	- "JavaScript setAttribute"
 
-Hint #2: You'll need to use the this keyword to access the name of the current animal from within the appear method to create the image path. The format to do so should look like this:
-	'images/' + this.name + '.jpg'
 
-Hint #3: Still stuck? The code to update the image source should look like this:
-	document.querySelector('img').setAttribute('src', 'images/' + this.name + '.jpg');
-*/
-	
+    var duck = {
+        name: 'duck'
+        friends: []
+
+    };
+
+    var swan = {
+        name: 'swan',
+        noise: 'chirp quack'
+    };
+
+
+    var makeFriend = function(newFriend) {
+        this.friends.push(newFriend);
+    }
+
+    duck.makeFriend = makeFriend;
+    duck.makeFriend(swan);
+
+    console.log(duck.friends);
+    // => { name: 'swan' }
+
+    duck.friends.makeNoise();
+    // => "chirp quack"
+
+
+
+    /* BONUS
+    This bonus will be a challenge! To complete this step, you'll need to do some Googling to look up things that have not yet been covered.
+
+    Take a look at the index.html file. You'll notice the following code:
+
+    	<img src="">
+    	
+    Here we have an image without a src attribute. 
+
+    Now take a peek inside the images folder to see what images are there. 
+
+    Here are the image paths for reference:
+
+    	- images/bird.jpg
+    	- images/cat.jpg
+    	- images/dog.jpg
+    	- images/mouse.jpg
+
+    Note that the name of each image is the animal name (bird, cat, dog, or mouse) plus an extension of .jpg.
+
+    Now that you're familiar with the code and assets that are available to you, let's add an appear property to each animal. The appear property will be a method that updates the src attribute to the path of the image of that animal.
+
+    Test things out by calling the appear method for one of your animals. When you load the page in your browser, the image of the animal should appear!
+
+    Hint #1: Google the following:
+    	- "JavaScript querySelector"
+    	- "JavaScript setAttribute"
+
+    Hint #2: You'll need to use the this keyword to access the name of the current animal from within the appear method to create the image path. The format to do so should look like this:
+    	'images/' + this.name + '.jpg'
+
+    Hint #3: Still stuck? The code to update the image source should look like this:
+    	document.querySelector('img').setAttribute('src', 'images/' + this.name + '.jpg');
+    */
+
+    document.querySelector(this.animal) style.background - image = "img src images/ + this.name + .jpg"
